5ab75b1c9276fff9c782097d5de4be8e91d9b5d3
mpracek <mpracek299@gmail.com>
2017-08-04 14:09:04 +0200

    Add files via upload

--- program.py
+++ program.py
@@ -1,57 +1,80 @@
-import urllib .request
-
-podatki = urllib.request.urlopen('http://www.flashscores.co.uk/')
-print(podatki)
-
-
-
-
 "Python 3.6.0 (v3.6.0:41df79263a11, Dec 23 2016, 07:18:10) [MSC v.1900 32 bit (Intel)] on win32"
+import os
+
+
 def verjetnost(ekipa, rezultati, kraj):
     '''Pove nam približno verjetnost, s katero bo prišlo do zmage, remija oziroma poraza glede na prejšnje rezultate ekipe.
     V rezultat vpišemo niz Z, P, R, kjer je Z zmaga, P poraz in R remi.
     V kraj vpišemo niz domač ali gost.'''
-    verjetnost1 = 0
-    for rezultat in rezultati:
-        if rezultat == 'Z':
-            verjetnost1 += 0.19
-        elif rezultat == 'R':
-            verjetnost1 += 0.1
-        elif rezultat == 'P':
-            verjetnost1 += 0.05
+    if len(rezultati) != 5:
+        return None
+    else: 
+        verjetnost1 = 0
+        for rezultat in rezultati:
+            if rezultat == 'Z':
+                verjetnost1 += 0.19
+            elif rezultat == 'R':
+                verjetnost1 += 0.1
+            elif rezultat == 'P':
+                verjetnost1 += 0.05
+            else:
+                verjetnost1 = verjetnost1
+        for _ in kraj:
+            if kraj == 'domač':
+                verjetnost1 * 1
+            elif kraj == 'gost':
+                verjetnost1 * 0.75
+        if verjetnost1 < 0.5:
+            return ekipa, verjetnost1, 'poraz ali remi'
         else:
-            return None
-    for _ in kraj:
-        if kraj == 'domač':
-            verjetnost1 * 1
-        elif kraj == 'gost':
-            verjetnost1 * 0.75
-    if verjetnost1 < 0.5:
-        return ekipa, verjetnost1, 'poraz ali remi'
-    else:
-        return ekipa, verjetnost1, 'zmaga'
+            return ekipa, verjetnost1, 'zmaga'
      
-        
 def seznam_stav(izhodna_datoteka, ekipa, rezultati, kraj):
-    '''V verjetnost vpišemo, kar nam vrne funkcija verjetnost, izhodna datoteka pa je datoteka, v katero bomo slikali'''
+    '''V izhodno datoteko zapišemo, verjetost nekega dogodka.'''
     with open (izhodna_datoteka, 'a') as dat:
-        print(verjetnost(ekipa, rezultati, kraj), file = dat)
+        print(verjetnost(ekipa, rezultati, kraj), file = dat)        
+
 
 def slovar_stav(vhodna_datoteka, izhodna_datoteka):
-    '''Vrne slovar stav'''
+    '''Verjetnosti uredi v slovar.
+>>> verjetnost('Hiša', 'PPRZZ', 'domač')
+('Hiša', 0.5800000000000001, 'zmaga')
+    slovar = {0.5800000000000001 :'Hiša', 'zmaga'}'''
+
     SlovarStav = {}
     with open (vhodna_datoteka) as dat2:
         with open (izhodna_datoteka, 'a') as dat3:
             for vrstica in dat2:
                 SlovarStav[vrstica[1]] = SlovarStav[vrstica[0][2]]
-        print(SlovarStav, file = 'dat3')
+        print(SlovarStav, file = dat3)
                 
+def skupna_stava(vhodna_datoteka):
+    '''Pokaže nam verjetnost, da se zgodijo vsi dogodki'''
+    with open(vhodna_datoteka) as vhod:
+        verjetnost_celote = 1
+        for vrstica in vhod:
+            razdelitev = vrstica.split(',')
+            verjetnost_celote = verjetnost_celote * float(razdelitev[1])
+    return verjetnost_celote
+
+def celota(vhodna_datoteka):
+    with open(vhodna_datoteka, 'a') as vhod:
+       print(skupna_stava(vhodna_datoteka), file = vhod)
+
+def izbris(vhodna_datoteka):
+    os.remove(vhodna_datoteka)
+
+def poraz():
+    return  'P'
+
+def remi():
+    return  'R'
+
+def zmaga():
+    return 'Z'
+
+
+    
+          
+            
         
-def uredi_po_vrednostih(slovar):
-    pari_vrednost_kljuc = []
-    for kljuc, vrednost in slovar.items():
-        pari_vrednost_kljuc.append((-vrednost, kljuc))
-    urejeno_po_vrednostih = []
-    for vrednost, kljuc in sorted(pari_vrednost_kljuc):
-        urejeno_po_vrednostih.append((kljuc, -vrednost))
-    return urejeno_po_vrednostih
--- vmesnik.py
+++ vmesnik.py
@@ -1,19 +1,50 @@
-import program
+import program as pr
 import tkinter as tk
 
-def odpri():
-    
+#def odpri():
+ #   okno = tk.Tk()
+  #  okno.mainloop()
 
-    window = tk.Toplevel(okno)
+#window = tk.Toplevel(okno)
+#gumb = tk.Button(okno, text='''S klikom na ta gumb razumem in se strinjam,
+#da pisec tega programa ni kriv za kakršno koli fizično ali psihično škodo''' ,
+#                command = odpri)
+#gumb.pack()
+
 
 okno = tk.Tk()
-gumb = tk.Button(okno, text='''S klikom na ta gumb razumem in se strinjam,
-da pisec tega programa ni kriv za kakršno koli fizično ali psihično škodo''' ,
-                 command = odpri)
-gumb.pack()
+okno.title('Mali stavni pomočnik')
+#okno.geometry('300x300')
 
+vpis = tk.Entry(okno)
+vpis.grid(row =  1,column = 0 )
+
+gumb1 = tk.Button(okno, text = 'Poraz', command = pr.poraz())
+gumb1.grid(row = 0, column = 1)
+gumb2 = tk.Button(okno, text = 'Zmaga', command = pr.zmaga())
+gumb2.grid(row = 1, column = 1)
+gumb3 = tk.Button(okno, text = 'Remi', command = pr.remi())
+gumb3.grid(row = 2, column = 1)
+gumb4 = tk.Button(okno, text = 'Igra doma')
+gumb4.grid(row = 0, column = 2)
+gumb5 = tk.Button(okno, text = 'Igra v Gosteh')
+gumb5.grid(row = 1, column = 2)
+gumb6 = tk.Button(okno, text = 'Dodaj')
+                  #command = pr.seznam_stav(tk.Entry())
+gumb6.grid(row = 1, column = 3)
+gumb7 = tk.Button(okno, text = 'Izpiši', command = pr.celota('stava.txt'))
+gumb7.grid(row = 1, column = 4)
+gumb8 = tk.Button(okno, text = 'Izbriši', command = pr.izbris('stava.txt'))
+gumb8.grid(row = 1, column = 5)
+
+#oznaka1 = tk.Label(okno, text = '''Izberi zadnjih pet rezultatov
+ #                                   med možnostmi poraz, zmaga ali remi.''')
+#oznaka1.grid(row = 2, column = 2 )
+#oznaka2 = tk.Label(okno, text = '''Med možnostima igra doma ter igra v gosteh
+ #                                   izberi tisto, ki drži.''')
+#oznaka2.grid(row = 2 ,column = 4 )
+
+vpis = tk.Entry(okno)
+vpis.grid(row =  1,column = 0 )
 
 okno.mainloop()
-
-
-
